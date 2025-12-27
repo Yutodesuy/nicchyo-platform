5c778cb4a9f9df80bf1ac48b02079d31ebc43c42
あなたの名前 <GitHub>
2025-12-27 22:27:23 +0900

    ショップバナーのフォントの大きさ調整、内容変更

--- app/(public)/search/SearchClient.tsx
+++ app/(public)/search/SearchClient.tsx
@@ -160,7 +160,7 @@ export default function SearchClient() {
             onClose={() => setSelectedShop(null)}
           />
           {canNavigate && (
-            <div className="fixed bottom-28 left-1/2 z-[2100] flex -translate-x-1/2 gap-3">
+            <div className="fixed bottom-20 left-1/2 z-[2100] flex -translate-x-1/2 gap-3">
               <button
                 type="button"
                 onClick={() => handleSelectByOffset(-1)}
