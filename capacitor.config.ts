064283fd2e2c1c8613dd3b1460b040a8431d1f3b
あなたの名前 <GitHub>
2025-12-28 20:19:44 +0900

    キャパシターによるアプリ化のためのセットアップ途中

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'jp.nicchyo.app',
+  appName: 'nicchyo',
+  webDir: 'out'
+};
+
+export default config;
